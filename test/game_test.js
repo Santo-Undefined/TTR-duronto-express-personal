3d3224219917f927ba17bb934f8564a2112e4847
Pradip Jana <janaprdip501@gmail.com>
2026-03-30 16:40:46 +0530

    [#8] | simplify ticketChoices to store only ticket IDs | Ayush/Pradip

--- test/game_test.js
+++ test/game_test.js
@@ -73,11 +73,7 @@ describe("testing the game", () => {
         "pink": 2,
         "red": 1,
       },
-      ticketChoices: [
-        { id: "t3", src: "Chicago", dest: "New Orleans", points: 7 },
-        { id: "t4", src: "Denver", dest: "El Paso", points: 4 },
-        { id: "t5", src: "Winnipeg", dest: "Little Rock", points: 11 },
-      ],
+      ticketChoices: ["t3", "t4", "t5"],
       bogies: 45,
     });
   });
