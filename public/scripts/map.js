e240c01f6c49b9fe206386696162306582098e8d
Santo K M <santokm@Santos-MacBook-Pro.local>
2026-03-31 14:29:07 +0530

    creating the routes and stations from script

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -6,34 +6,6 @@ const CITIES = {
   "Salt Lake City": "SLC",
   "Boston": "BSN",
   "Charleston": "CLN",
-  "Nashville": "NVL",
-  "Pittsburgh": "PBH",
-  "Duluth": "DLT",
-  "New Orleans": "NOL",
-  "Raleigh": "RGH",
-  "El Paso": "ELP",
-  "Phoenix": "PHX",
-  "Santa Fe": "SFE",
-  "Denver": "DVR",
-  "Helena": "HLN",
-  "New York": "NYC",
-  "Miami": "MIM",
-  "St Louis": "SLS",
-  "Toronto": "TRT",
-  "Kansas City": "KCT",
-  "Houston": "HTN",
-  "Calgary": "CLC",
-  "Winnipeg": "WPG",
-  "Las Vegas": "LVS",
-  "Oklahoma City": "OKC",
-  "Sault St. Marie": "SSM",
-  "Montreal": "MTL",
-  "Washington": "WDC",
-  "Atlanta": "ATL",
-  "Chicago": "CHG",
-  "Omaha": "OMH",
-  "Little Rock": "LRK",
-  "Dallas": "DLS",
 };
 
 const claimRoute = (e) => {
@@ -51,15 +23,17 @@ const claimRoute = (e) => {
 
 const loadRoute = (map) => {
   const data = [
-    { id: "route-STL-CLC", trackColor: "transparent" },
-    { id: "route-HLN-CLC", trackColor: "transparent" },
-    { id: "route-STL-HLN", trackColor: "yellow" },
-    { id: "route-STL-VCR", trackColor: "transparent" },
-    { id: "route-VCR-CLC", trackColor: "transparent" },
+    { id: "STL-CLC", trackColor: "transparent" },
+    { id: "HLN-CLC", trackColor: "red" },
+    { id: "STL-HLN", trackColor: "yellow" },
+    { id: "STL-VCR", trackColor: "transparent" },
+    { id: "VCR-CLC", trackColor: "transparent" },
   ];
 
   data.forEach((route) => {
     const track = map.querySelector(`#${route.id}`);
+
+    track.setAttribute("class", "route");
     track.setAttribute("data-route-color", route.trackColor);
     track.setAttribute("data-owner-color", "none");
   });
@@ -72,26 +46,35 @@ const createStations = () => {
   const stationRef = document.querySelector("#station_ref");
   Object.entries(CITIES).forEach(([name, short]) => {
     const station = stationRef.cloneNode(true);
-    station.id = short.toUpperCase();
+    station.id = short;
     const text = station.querySelector("tspan");
-    text.textContent = name;
+    text.textContent = name.toUpperCase();
     station.setAttribute("transform", `translate(${posX}, ${posY += 20})`);
+    station.setAttribute("inkscape:label", `stn-${short}`);
+
     container.appendChild(station);
   });
 };
 
-const ROUTES = ["HLN-MIM", "STL-CLC", "VCR-CLC"];
+const ROUTES = [
+  "STL-CLC",
+  "HLN-CLC",
+  "STL-HLN",
+  "STL-VCR",
+  "VCR-CLC",
+];
 
 const createRoutes = () => {
   const posX = 0;
   let posY = 30;
   const container = document.querySelector("#routes_and_stations");
   const routeRef = document.querySelector("#route_ref");
-  console.log(routeRef);
   ROUTES.forEach((id) => {
     const route = routeRef.cloneNode(true);
     route.id = id;
+    route.setAttribute("inkscape:label", `route-${id}`);
     route.setAttribute("transform", `translate(${posX}, ${posY += 30})`);
+    route.removeAttribute("style");
     container.appendChild(route);
   });
 };
@@ -99,7 +82,7 @@ const createRoutes = () => {
 globalThis.onload = async () => {
   const mapContainer = document.querySelector("#map");
 
-  await fetch("/assets/map.svg")
+  await fetch("/assets/map2.svg")
     .then((res) => res.text())
     .then((data) => {
       const parser = new DOMParser();
@@ -110,10 +93,12 @@ globalThis.onload = async () => {
   createStations();
   createRoutes();
 
-  const routes = document.querySelectorAll(".route");
-
   loadRoute(mapContainer);
-  routes.forEach((route) => {
-    route.addEventListener("click", claimRoute);
-  });
+  mapContainer.addEventListener("click", claimRoute);
+  // const routes = document.querySelectorAll(".route");
+  // console.log([...routes]);
+  // routes.forEach((route) => {
+  //   console.log(route);
+  //   route.addEventListener("click", claimRoute);
+  // });
 };
