0c27029ad57ceae640c48fb5ee2a222fe5bc1ec4
Pradip Jana <janaprdip501@gmail.com>
2026-03-31 12:26:29 +0530

    [#9] | adds test case for checking the wild card more than or equal to 3 in face-up deck | Pradip/Ayush

--- test/train_card_deck_test.js
+++ test/train_card_deck_test.js
@@ -98,6 +98,86 @@ describe("train car card deck", () => {
     ]);
   });
 
+  it("reopen faceup card when there are more than or equal to 3 wild cards ", () => {
+    const deck = [
+      "white",
+      "wild",
+      "wild",
+      "black",
+      "wild",
+      "blue",
+      "green",
+      "orange",
+      "white",
+      "blue",
+      "blue",
+      "green",
+      "white",
+    ];
+
+    const trainCardDeck = new CarCardsDeck(deck);
+    trainCardDeck.initFaceUp();
+
+    assertEquals(trainCardDeck.faceUp, [
+      "blue",
+      "green",
+      "orange",
+      "white",
+      "blue",
+    ]);
+    assertEquals(trainCardDeck.faceDown, ["blue", "green", "white"]);
+  });
+
+  it("draw one card from faceup [check: the faceup wild >= 3]", () => {
+    const deck = [
+      "white",
+      "orange",
+      "wild",
+      "green",
+      "wild",
+      "wild",
+      "green",
+      "orange",
+      "green",
+      "orange",
+      "white",
+      "blue",
+      "blue",
+      "green",
+      "white",
+    ];
+
+    const trainCardDeck = new CarCardsDeck(deck);
+    trainCardDeck.initFaceUp();
+    assertEquals(trainCardDeck.faceUp, [
+      "white",
+      "orange",
+      "wild",
+      "green",
+      "wild",
+    ]);
+    assertEquals(trainCardDeck.faceDown, [
+      "wild",
+      "green",
+      "orange",
+      "green",
+      "orange",
+      "white",
+      "blue",
+      "blue",
+      "green",
+      "white",
+    ]);
+    assertEquals(trainCardDeck.drawCardFromFaceUp("2"), "orange");
+    assertEquals(trainCardDeck.faceUp, [
+      "green",
+      "orange",
+      "green",
+      "orange",
+      "white",
+    ]);
+  });
+
   it("draw one card from deck", () => {
     const deck = [
       "white",
